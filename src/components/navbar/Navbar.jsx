ed520c040dd904cc01212fd24f11dd3515e5c9f7
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-03-02 20:42:34 +0100

    020322 navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/GPT-3.svg'
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src = {logo} alt="logo" />
+        </div>
+        <div className='gpt3__navbar-lonks_container'>
+          <p><a href="#home"> Home</a></p>
+          <p><a href="#wpt3"> What is GPT3</a></p>
+          <p><a href="#home"> Open AI</a></p>
+          <p><a href="#features"> Case Studies</a></p>
+          <p><a href="#blog"> Library</a></p>
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+    </div>
   )
 }
 
